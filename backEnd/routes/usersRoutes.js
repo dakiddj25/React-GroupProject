ed7eb7dddfb2d4de99d5ce8a7b0f9af1c717d936
John Jones Jr <jayjohnjones@gmail.com>
2020-03-04 00:08:41 -0500

    routes created

---
+++ backEnd/routes/usersRoutes.js
@@ -0,0 +1,18 @@
+const users = require("express").Router();
+const { getSingleUser ,getUser, createUser, editUser, deleteUser } = require("../../queries/users");
+
+
+
+users.get("/:id", getSingleUser);
+
+users.post("/:username", getUser); // get user by username
+
+users.post("/", createUser); 
+
+users.patch("/:id", editUser);
+
+users.delete("/:id", deleteUser);
+
+
+module.exports = users;
+
