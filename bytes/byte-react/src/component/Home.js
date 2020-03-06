71632d5b629a0fd0bcf7067bd2090d8bfc8e3602
John Jones Jr <jayjohnjones@gmail.com>
2020-03-06 13:27:38 -0500

    Home page

--- bytes/byte-react/src/component/Home.js
+++ bytes/byte-react/src/component/Home.js
@@ -1,11 +1,67 @@
 import React, {useEffect, useState} from "react";
+import axios from "axios"
 
-const Home = () => {
+
+const getUserProfilePic = async()=> {
+    try{
+        let res = await axios.get("http://localhost:3001/users/1")
+       return res.data.payload.user_pic
+
+    } catch (err){
+        debugger
+        console.log(err)
+    }
+}
+
+const displayProfilePic = ()=> {
+        let propic = getUserProfilePic()
+        
+        return (
+            <div className = "profilePic">
+                <img src = {propic}></img>
+                <button>Create a post</button>
+            </div>
+        )
+
+}
+
+
+
+const getFeeds = async()=> {
+    try{
+        let res = await axios.get("http://localhost:3001/posts")
+        debugger
+
+
+    } catch (err){
+        debugger
+        console.log(err)
+    }
+}
+
+const displayFeed = () => {
+    let feeds = getFeeds()
     return (
-        <div>
-            Home
+        <div className = "feeds">
+            <img src = {feeds}></img>
         </div>
     )
 }
 
-export default Home;
+
+
+
+
+
+const display = () => {
+    const profile = displayProfilePic();
+    debugger
+    return (
+        <div>
+            Home
+            {profile}
+        </div>
+    )
+}
+
+export default display;
