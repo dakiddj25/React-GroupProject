8fbb969861b097c3acf6be3308ef5e0c0c9c12f0
rafidhossain1995 <rafidhossain@pursuit.org>
2020-03-07 11:39:26 -0500

    signup page connects to database

--- bytes/byte-react/src/component/SignUp.js
+++ bytes/byte-react/src/component/SignUp.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useInputs } from "../utility/customHooks";
 import { useHistory } from "react-router-dom";
 import "../css/SignUp.css"
+import axios from "axios";
 
 const SignUp = () => {
     const firstName = useInputs("")
@@ -13,19 +14,36 @@ const SignUp = () => {
 
     const history = useHistory()
     
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        try{
+            let res = await axios.post("http://localhost:3001/users", {
+              firstName: firstName.value,
+              lastName: lastName.value,
+              userName: userName.value,
+              password: password.value,
+              email: email.value,
+              user_pic: userPic.value
+          })
+          debugger
+        }catch(err){
+            console.log(err)
+        }
+    }
 
 
 
 
     return (
         <div className="body">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="First Name" required {...firstName}/>
                 <input type="text" placeholder="Last Name" required {...lastName}/>
                 <input type="text" placeholder="User Name" required {...userName}/>
                 <input type="text" placeholder="Password" type="password" required {...password}/>
                 <input type="text" placeholder="Email" required {...email}/>
                 <input type="text" placeholder="Upload Photo" {...userPic}/>
+                <input type="submit"/>
             </form>
         </div>
     )
