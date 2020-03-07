39e0a95e1e7d37e372264d868c3e4000b917df58
rafidhossain1995 <rafidhossain@pursuit.org>
2020-03-07 17:12:13 -0500

    finish signup page

--- bytes/byte-react/src/component/LogIn.js
+++ bytes/byte-react/src/component/LogIn.js
@@ -1,9 +1,30 @@
 import React, {useEffect, useState} from "react";
+import { useInputs, useStateWithLocalStorage } from "../utility/customHooks";
+import image from './../css/Assets/bytesLogo.jpg'
+import image2 from '../css/Assets/group.jpg'
 
 const LogIn = () => {
+    const userName = useInputs("")
+    const password = useInputs("")
+
     return (
-        <div>
-            LogIn
+        <div class="grid-container">
+            <div class="GroupPicture">
+            <img src={image2} alt="" className="group"/>
+            </div>
+
+             <div class="SignUp">
+             <form className="signUp">
+                <img src={image} alt="" className="logo"/> 
+                <h1> Log In To Byte Into Your Friend's Culinary Arts. </h1>
+                <input type="text" placeholder="User Name" required {...userName}/>
+                <input type="text" placeholder="Password" type="password" required {...password}/>
+                <h5>Upload Profile Picture</h5>
+               Login <input type="submit" className="submit"/>
+                
+            </form>
+
+             </div>
         </div>
     )
 }
