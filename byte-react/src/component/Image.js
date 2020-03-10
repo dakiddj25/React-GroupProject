dbc6472b10051b177ffd2f777a35943f7349fe91
crystal cardona <crystalcardona@pursuit.org>
2020-03-09 22:07:51 -0400

    updated Profile

--- byte-react/src/component/Image.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Image = ({filePath}) => {
-    return <img src={filePath} alt="" />
-}
-
-export default Image;
--- byte-react/src/component/Profile.js
+++ byte-react/src/component/Profile.js
@@ -1,13 +1,17 @@
 import React,{useEffect, useState} from "react";
-import Image from './Image'
 import axios from 'axios';
 import "../css/Profile.css";
 import image from './../css/Assets/bytesLogo.jpg';
+import { useInputs } from "../utility/InputHooks";
+
 
 const Profile = () => {
 
     const [info, setInfo] = useState([])
     const [feed, setFeed] = useState([])
+    const captions = useInputs("")
+    const pictures = useInputs("")
+    const user_id = useInputs("")
 
 
 
@@ -18,7 +22,7 @@ const Profile = () => {
     const fetchUserInfo = async () => {
         let user = localStorage.getItem("currentUser")
         try {
-            let res = await axios.get("http://localhost:3001/users/1")
+            let res = await axios.get(`http://localhost:3001/users/1`)
             setInfo(Object.values(res.data))
         } catch(err){
             console.log(err)
@@ -43,12 +47,30 @@ const Profile = () => {
             fetchUsersFeed()
         }, [])
 
+    const createNewPost = async () => {
+        try {
+            let res = await axios.post("http://localhost:3001/posts/", {
+                user_id: user_id.value,
+                pictures: pictures.value,
+                captions: captions.value,
+            })
+            debugger
+
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        createNewPost()
+    })
+
     
     let  showInfo = info.map(user => {
         return (
         <div><h2>{user.username}</h2>
         <img src={user.user_pic} />
-        <h3>{user.firstname} {user.lastname}</h3>
+        <h3>{user.firstname}{user.lastname}</h3>
         <p>{user.email}</p>
         </div>
         )
@@ -65,22 +87,12 @@ const Profile = () => {
             </div>
             <div className="Banner"></div>
             <div className="UserInfo">
-                {/* <div className="userName">
-                    UserName
-                </div>
-                <div className="profilePic">
-                    ProfilePic
-                </div>
-                <div className="userInfo">
-              
-                </div> */}
                 {showInfo}
             </div>
             <form className="UserFeed">
-                <input type="text" placeholder="Enter A Caption!"/>
-                <input type="file" accept="image/*" />
-                <button> Submit </button>
-
+                <input type="text" placeholder="Enter A Caption!" {...captions}/>
+                <input type="file" accept="image/*"/>
+                <button> Post Bytes </button>
                 {showFeed}
             </form>
             <div class="Empty"></div>
