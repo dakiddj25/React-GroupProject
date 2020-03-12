3bea3e23cbd5dbb0993052b0de2d89d2101ffaf9
crystal cardona <crystalcardona@pursuit.org>
2020-03-12 01:24:16 -0400

    fixing bug

--- byte-react/src/component/Home.js
+++ byte-react/src/component/Home.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import { useInputs, fetchData } from "../utility/InputHooks";
 import SearchBar from "./helpme/hashtagSearch"
 import FeedIndex from "./helpme/feedIndex"
+import UserDisplay from "./helpme/userInfo"
 import "../css/home.css"
 import addPost from './helpme/addPost'
 
@@ -10,11 +11,13 @@ const Home = () => {
 
 
     const [posts, setPosts ] = useState([])
+    const [userInfo, setUserInfo ] = useState([])
     
     const getPosts = async (str="") =>{
         try{
             let res = await axios.post("http://localhost:3001/hashtag/getHashtag", {hashtag: str})
             setPosts(res.data.payload)
+            debugger
         }catch(err){
             console.log(err)
             setPosts([])
@@ -25,12 +28,35 @@ const Home = () => {
         getPosts()
     },[])
     const handleSubmit = (str)=>{getPosts(str)}
+
+    const getUserProfile = async () =>{
+        try{
+            let user = localStorage.getItem("currentUserID")
+           
+            let res = await axios.get(`http://localhost:3001/users/${user}`)
+      
+            setUserInfo(res.data.payload)
+            debugger
+           // set it in the state
+         } catch (err){
+            debugger
+            console.log(err)
+            setUserInfo([])
+
+        }
+     }
+
+     useEffect(()=>{
+        getUserProfile()
+    },[])
+     
     
 
     return(
         <>
-        <SearchBar handleSubmit={handleSubmit}/>
-        <FeedIndex posts={posts}/>
+        <UserDisplay userInfo = {userInfo} />
+        <SearchBar handleSubmit={handleSubmit} />
+        <FeedIndex posts={posts} />
         </>
 
     )
