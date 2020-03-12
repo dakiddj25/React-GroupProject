2e93c344319d52166f590eab395da21a150d7b95
John Jones Jr <jayjohnjones@gmail.com>
2020-03-11 21:55:56 -0400

    UserInfo on Home Page

---
+++ byte-react/src/component/helpme/userInfo.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react"
+import addPost from "../helpme/addPost"
+
+const UserDisplay =({userInfo})=>{
+  debugger
+   
+        return(
+
+            <div className = "profilePic" key = {userInfo.id}>
+                <h2>{userInfo.username}</h2>
+               <img src = {userInfo.user_pic}></img>
+              
+               <form onSubmit = {addPost}>
+                   <input name= "caption" type = "text" placeholder = "caption"/>
+                   <input name = "image" type= "file" accept = "image/*"/>
+                   <button>submit</button>
+               </form>
+           </div>
+
+        )
+  
+  
+
+}
+export default UserDisplay
